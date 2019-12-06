5dde77876834e93305d2803dc7c89c35f128a4f2
Matias Ruiz <matias@acomeradmin.cl>
2019-12-05 23:37:46 -0300

    New landing

---
+++ layouts/default.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <section class="main-content columns">
+      <div class="container column is-12">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  },
+  created: function() {
+    setTimeout(() => {
+      location.replace('https://growonline.cl/')
+    }, 2500);
+  }
+}
+</script>
+
+<style>
+  .primary_color{
+    color: #09ad2a;
+  }
+  .section {
+    padding: 80px 0;
+  }
+  h2 {
+    margin-bottom: 35px;
+    width: 100%;
+    float: left;
+  }
+  h3{
+    width: 100%;
+    margin-top: 10px!important;
+    float: left;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="section">
+    <h2 class="title is-1 has-text-grey has-text-centered">
+      Ahora somos <a href="https://growonline.cl/" class="primary_color">Grow Online
+      </a>
+    </h2>
+    <h3 class="subtitle is-5 has-text-grey has-text-centered">
+      Hemos creado un <a href="https://growonline.cl/" target="_blank" class="primary_color">nuevo sitio</a> junto a una nueva
+      marca para darte el mejor servicio y los mejores productos para tu cultivok, en el mejor sitio web de growshop de Chile.
+    </h3>
+
+    <h3 class="subtitle is-5 has-text-grey has-text-centered">
+      Te estamos redirigiendo a nuestro nuevo sitio...
+    </h3>
+  </section>
+</template>
